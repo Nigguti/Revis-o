4cf37c0f85eca621cb83990e5188de2826743539
Nigguti <164106348+Nigguti@users.noreply.github.com>
2024-04-03 11:49:14 +0000

    03.04.2024 - 8:50

---
+++ src/tarefa/controllers/tarefa.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import tarefaService from "../services/tarefa.services;
+
+class tarefaController {
+    async create(req: Request, res: Response) {
+        const createdTarefa = await tarefaService.create(req.body)
+        res.status(201)
+        return res.json(createdTarefa)
+    }
+    async findAll(req: Request, res: Response) {
+        const findedTarefas = await tarefaService.find(req.params.id)
+        return res.json(findedTarefas)
+    }
+    async findById(req: Request, res: Response) {
+        const findedTarefa = await tarefaService.findById(req.params.id)
+        return res.json(findedTarefa)
+    }
+    async update(req: Request, res: Response) {
+        try {
+            const uptadeTarefa = await tarefaService.findByIdAndUpdate.update(req.params.id, req.body)
+            return res.json(uptadeTarefa)
+        } catch (error) {
+            throw new Error(`Atualização não realizada:${error}`)
+        }
+    }
+    async delete(req: Request, res: Response){
+        try {
+            const deleteTarefa = await tarefaService.findByIdAndDelete.delete(req.params.id)
+            return res.json(deleteTarefa)
+        } catch (error) {
+            throw new Error(`A tarefa não foi deletada:${error}`)
+        }
+    }
+}

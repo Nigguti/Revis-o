7e53e4246390aec0decbadb7a9c19e6e1c98a9b0
Nigguti <164106348+Nigguti@users.noreply.github.com>
2024-04-04 00:07:11 +0000

    03.04.2024 - 21:10

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router} from 'express'
 import userController from './usuario/controllers/user.controller'
+import categoriaController from './categoria/controllers/categorias.controllers'
+import tarefaController from './tarefa/controllers/tarefa.controller'
 
 const routes = Router()
 
@@ -8,3 +10,15 @@ routes.get('/users', userController.findAll)
 routes.get('/user/:id', userController.findById)
 routes.put('/user/:id', userController.update)
 routes.delete('/user/:id', userController.delete)
+
+routes.post('/categorias', categoriaController.create)
+routes.get('/categorias', categoriaController.findAll)
+routes.get('/categorias', categoriaController.findByid)
+routes.put('/categorias', categoriaController.update)
+routes.delete('/categorias', categoriaController.delete)
+
+routes.post('/tarefas', tarefaController.create)
+routes.get('/tarefas', tarefaController.findAll)
+routes.get('/tarefas', tarefaController.findById)
+routes.put('/tarefas', tarefaController.update)
+routes.delete('/tarefa', tarefaController.delete)
